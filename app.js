722dc11fe02aa59551b72b2cfaae73954ac852c4
REEVE <nreevevi6512@gmail.com>
2020-08-19 18:46:28 +0800

    1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const session = require("express-session");
+const mongoose = require("mongoose");
 const app = express();
 
 //M EJS
@@ -10,6 +11,13 @@ app.set("view engine", "ejs");
 app.use("/", require("./routes/index"));
 app.use("/user", require("./routes/user"));
 
+//COnnect To DB
+mongoose.connect(
+  process.env.DB_connect,
+  { urlNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to DB")
+);
+//LISTEN
 app.listen(3100, function () {
   console.log("Server Connected");
 });
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 //Login
-router.get("/login", (req, res) => res.send("LogIn"));
+router.get("/login", (req, res) => res.render("LogIn"));
 
 //Register
-router.get("/Register", (req, res) => res.send("Register"));
+router.get("/Register", (req, res) => res.render("Register"));
 
 module.exports = router;
